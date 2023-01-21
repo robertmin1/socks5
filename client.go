3f6206663bcddb5817ed3c0365fdece357b20eef
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-01-21 14:08:20 +0300

    Test

--- client.go
+++ client.go
@@ -32,7 +32,6 @@ func NewClient(addr, username, password string, tcpTimeout, udpTimeout int) (*Cl
 		TCPTimeout: tcpTimeout,
 		UDPTimeout: udpTimeout,
 	}
-	c.Password = "WORKS"
 	return c, nil
 }
 
